e62a650c9ebe5ad21d92fb498c96c7a17433b303
Waleed Azhar <waleedazhar386@gmail.com>
2018-01-13 02:08:37 +0500

    Initial commit

---
+++ app/src/main/java/com/example/waleedazhar/fragments/Front_frangment.java
@@ -0,0 +1,20 @@
+package com.example.waleedazhar.fragments;
+
+ import android.os.Bundle;
+ import android.support.annotation.Nullable;
+ import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+/**
+ * Created by Waleed Azhar on 10/16/2017.
+ */
+
+public class Front_frangment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.frontfragment, container, false);
+        return view;
+    }
+}
